f587b98b44d1d8ca6de6deeda92ea1cc1009c8dd
vagrant <vagrant@vagrant.vm>
2017-06-19 04:45:22 +0000

    スキーマ変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619023657) do
+ActiveRecord::Schema.define(version: 20170619030845) do
+
+  create_table "color", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string "color_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "plans", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "title"
