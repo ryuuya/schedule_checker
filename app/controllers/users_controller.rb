b5efef88e65182c9b56d8e40c4bd857a8d4d1122
Your Name <you@example.com>
2017-06-19 04:33:55 +0000

    newファイル記入欄作成、ルーティング設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def new
     @new = User.new
   end
+  def create
+    @user = User.new (user_params)
+    @user.save
+    redirect_to users_index_path
+  end
 end
