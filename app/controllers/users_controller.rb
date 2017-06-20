333319b461cbd035c175741cd1aafcb5e950522b
Your Name <you@example.com>
2017-06-20 06:17:11 +0000

    ログイン画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.save
-    redirect_to users_index_path
+    redirect_to index_path
   end
+  #def login
+   # @user = User.find_by_login_id params[:login_id]
+    #if @user && @user.authenticate(params[:password])
+     #   render :text => "Login OK"
+   # else
+    #    render :text => "Login NG"
+   # end
+  #end
   private
    def user_params
      params[:user].permit(:login_id, :name, :password, :password_confirmation, :address)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :plans
   has_secure_password
 end
