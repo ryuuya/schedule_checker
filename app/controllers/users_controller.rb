2e3b1850d137bb0255e07242ceb19efcef2526c9
Your Name <you@example.com>
2017-06-20 02:22:13 +0000

    住所のセレクトボックス作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,10 @@ class UsersController < ApplicationController
     @user.save
     redirect_to users_index_path
   end
+  def login 
+  end
   private
    def user_params
-     params[:user].permit(:login_id, :name, :address, :password)
+     params[:user].permit(:login_id, :name, :password, :address)
    end
 end
