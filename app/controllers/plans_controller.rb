86c4369792cd2265e8395cd4652cfb491c12c7c3
vagrant <vagrant@vagrant.vm>
2017-06-21 02:53:02 +0000

    アサイド追加

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,15 +1,21 @@
 class PlansController < ApplicationController
   def index
-    plans = Plan.all
-    
+    @plans = Plan.all
+    today = Time.now
     #カレンダーセット用データ
     @datas = []
-    plans.each do |date| 
-      @datas = [
+    @today_plans = []
+    @plans.each do |date| 
+      @datas += [
         'title' => date['title'],
         'start' => date['start_at'],
+        'end' => date['end_at'],
         'url' => '/show/' + date['id'].to_s
       ]
+      p today.strftime("%m%d")
+      if today.strftime("%m%d") == date['start_at'].strftime("%m%d")
+        @today_plans.push(date['title'])
+      end
     end
   end
 
