2d04b077728a0879df5574e1067762a93fcd51aa
vagrant <vagrant@vagrant.vm>
2017-06-23 07:34:43 +0000

    スケジュールカラー反映

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -41,7 +41,8 @@ class PlansController < ApplicationController
         'title' => date['title'],
         'start' => date['start_at'],
         'end' => date['end_at'],
-        'url' => '/show/' + date['id'].to_s
+        'url' => '/show/' + date['id'].to_s + "?user_id=" + params[:user_id],
+        'color' => "#" + date.color.color_code
       ]
       if today.strftime("%m%d") == date['start_at'].strftime("%m%d")
         @today_plans.push(date['title'])
